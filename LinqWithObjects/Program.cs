e5284b7f4ba572b7325661d0942a10660be3f84a
ryan <ryancumming7@gmail.com>
2022-05-24 15:27:33 +0100

    Initial commit, added methods to main in project

---
+++ LinqWithObjects/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqWithObjects
+{
+    class Program
+    {
+        static void LinqWithArrayOfStrings()
+        {
+            var names = new string[] {"Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
+            var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+
+            foreach (string item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static bool NameLongerThanFour(string name)
+        {
+            return name.Length > 4;
+        }
+
+        static void Main(string[] args)
+        {
+            LinqWithArrayOfStrings();
+        }
+    }
+}
