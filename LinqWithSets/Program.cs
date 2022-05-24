5cf4f9912fc58d48bccd89e1493f5ef4d3f593f7
ryan <ryancumming7@gmail.com>
2022-05-24 15:45:56 +0100

    Added LinqWithSets folder and project

---
+++ LinqWithSets/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic; // for IEnumerable<T>
+using System.Linq; // for Linq extension methods
+
+namespace LinqWithSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
