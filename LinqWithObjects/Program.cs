71da93d3598238412d24a168bf3944f33b9c2e75
ryan <ryancumming7@gmail.com>
2022-05-24 15:37:03 +0100

    Added queries on name array and lambda expression

--- LinqWithObjects/Program.cs
+++ LinqWithObjects/Program.cs
@@ -8,7 +8,18 @@ namespace LinqWithObjects
         static void LinqWithArrayOfStrings()
         {
             var names = new string[] {"Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
-            var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+            //Long way to call method
+            //var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+
+            //succint way to call method
+            //var query = names.Where(NameLongerThanFour);
+
+            /*Lambda - "nameless function"
+              lambda needs only: 
+                    - The names of input parameters
+                    - A return value expression*/
+
+            var query = names.Where(name => name.Length > 4).OrderBy(name => name.Length);
 
             foreach (string item in query)
             {
