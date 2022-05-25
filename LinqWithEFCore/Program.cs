77e2b6f70ee58844c0110aa1ff6652decbe41505
ryan <ryancumming7@gmail.com>
2022-05-25 11:28:21 +0100

    Added FilterAndSort method to program

--- LinqWithEFCore/Program.cs
+++ LinqWithEFCore/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using static System.Console;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace LinqWithEFCore
 {
     class Program
     {
+        static void FilterAndSort()
+        {
+            using (var db = new Northwind())
+            {
+                var query = db.Products
+                    //query is a DbSet<Product>
+                    .Where(product => product.UnitPrice < 10M)
+                    //query is now an IQueryable<Product>
+                    .OrderByDescending(product => product.UnitPrice);
+
+                WriteLine("Products that cost less than $10: ");
+                foreach (var item in query)
+                {
+                    WriteLine($"{item.ProductID}: {item.ProductName} costs {item.UnitPrice:$#,##0.00}");
+                }
+                WriteLine();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FilterAndSort();
         }
     }
 }
