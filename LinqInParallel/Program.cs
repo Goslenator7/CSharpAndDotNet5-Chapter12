4ec5dee48c4dc08261af6f9f2693e59537329929
ryan <ryancumming7@gmail.com>
2022-05-25 14:34:30 +0100

    Added LinqInParallel project, added to main method

--- LinqInParallel/Program.cs
+++ LinqInParallel/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
 
 namespace LinqInParallel
 {
@@ -6,7 +10,17 @@ namespace LinqInParallel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var watch = new Stopwatch();
+            Write("Press enter to start: ");
+            ReadLine();
+            watch.Start();
+
+            IEnumerable<int> numbers = Enumerable.Range(1, 2_000_000_000);
+
+            var squares = numbers.Select(number => number * number).ToArray();
+
+            watch.Stop();
+            WriteLine($" {watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
         }
     }
 }
